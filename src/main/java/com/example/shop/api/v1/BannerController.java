9f80086c42f2df5e363dca634fcf09331b549d3f
KokoTa <584847514@qq.com>
2020-03-12 22:13:00 +0800

    update: 参数校验

--- src/main/java/com/example/shop/api/v1/BannerController.java
+++ src/main/java/com/example/shop/api/v1/BannerController.java
@@ -4,11 +4,13 @@ import com.example.shop.dto.User;
 import com.example.shop.exception.http.ForbiddenException;
 import com.example.shop.other.testClass.A;
 import com.example.shop.other.testClass.B;
+import org.hibernate.validator.constraints.Length;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
 
 import javax.validation.constraints.Max;
+import javax.validation.constraints.Min;
 import java.util.Map;
 
 @RestController
@@ -27,13 +29,16 @@ public class BannerController {
     }
 
     @GetMapping("/testParams/{userId}")
-    Integer testParamByPath(@PathVariable(name = "userId") Integer id) {
+    Integer testParamByPath(@PathVariable(name = "userId") @Min(10) Integer id) {
         return id;
     }
 
     @GetMapping("/testParams")
-    Integer testParamByQuery(@RequestParam(name = "userId") Integer id) {
-        return id;
+    String testParamByQuery(
+            @RequestParam(name = "userId") @Min(10) Integer id,
+            @RequestParam(name = "userName") @Length(min = 2) String name
+    ) {
+        return id + " " + name;
     }
 
     @PostMapping("/testParams")
--- src/main/java/com/example/shop/core/GlobalExceptionHandle.java
+++ src/main/java/com/example/shop/core/GlobalExceptionHandle.java
@@ -7,12 +7,18 @@ import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.ObjectError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.ResponseStatus;
 
 import javax.servlet.http.HttpServletRequest;
+import javax.validation.ConstraintViolation;
+import javax.validation.ConstraintViolationException;
+import java.util.List;
+import java.util.Set;
 
 @ControllerAdvice
 public class GlobalExceptionHandle {
@@ -20,15 +26,18 @@ public class GlobalExceptionHandle {
     @Autowired
     private ExceptionCodeConfiguration exceptionCodeConfiguration;
 
+    // 全局异常
     @ExceptionHandler(value = Exception.class)
     @ResponseBody
     @ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
     public ExceptionResponse handleException(HttpServletRequest req, Exception e) {
         String method = req.getMethod();
         String url = req.getRequestURI();
-        return new ExceptionResponse(10000, "服务器异常", method + " " + url);
+        String errStr = e.getMessage();
+        return new ExceptionResponse(10000, errStr, method + " " + url);
     }
 
+    // 自定义的 Http 异常
     @ExceptionHandler(value = HttpException.class)
     @ResponseBody
     public ResponseEntity<ExceptionResponse> handleHttpException(HttpServletRequest req, HttpException e) {
@@ -47,4 +56,40 @@ public class GlobalExceptionHandle {
 
         return responseEntity;
     }
+
+    // body 参数的异常
+    @ExceptionHandler(value = MethodArgumentNotValidException.class)
+    @ResponseBody
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    public ExceptionResponse handleMethodArgumentNotValidException(HttpServletRequest req, MethodArgumentNotValidException e) {
+        String method = req.getMethod();
+        String url = req.getRequestURI();
+        // 可能有多个验证错误
+        List<ObjectError> errors = e.getBindingResult().getAllErrors();
+        String errMsg = this.formatAllErrorMessages(errors);
+
+        return new ExceptionResponse(10001, errMsg, method + " " + url);
+    }
+
+    private String formatAllErrorMessages(List<ObjectError> errors) {
+        StringBuffer msg = new StringBuffer();
+        errors.forEach(err -> {
+            msg.append(err.getDefaultMessage()).append("; ");
+        });
+        return msg.toString();
+    }
+
+    // 查询参数的异常
+    @ExceptionHandler(value = ConstraintViolationException.class)
+    @ResponseBody
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    public ExceptionResponse handleConstraintViolationException(HttpServletRequest req, ConstraintViolationException e) {
+        String method = req.getMethod();
+        String url = req.getRequestURI();
+        // 可能有多个验证错误
+        String errMsg = e.getMessage();
+
+        return new ExceptionResponse(10001, errMsg, method + " " + url);
+    }
+
 }
